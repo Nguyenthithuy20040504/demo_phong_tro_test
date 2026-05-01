48196c2ba52bff3d1135dcf7ab6fd18a807a3db9
Cong Nguyen Quoc <nt3thuy0504@gmail.com>
2026-05-02 01:29:08 +0700

    fix

--- list-all-buildings.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config({ path: '.env' });
-mongoose.connect(process.env.MONGODB_URI).then(async () => {
-  const ToaNha = mongoose.connection.collection('toanhas');
-  const items = await ToaNha.find({}).project({tenToaNha: 1}).toArray();
-  items.forEach(i => console.log(i.tenToaNha));
-  process.exit(0);
-});
--- tmp-fetch.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-
-async function check() {
-  const res = await fetch('http://localhost:3000/api/hoa-don', {
-    headers: {
-      cookie: 'next-auth.session-token=your-token-if-needed' // Wait, I might need to simulate session or query DB directly to see if mongoose hook changes it
-    }
-  });
-}
